87aa0725c7d0b151069eee49273ffd2014307ef9
BloopHD <mrobcarlton@gmail.com>
2023-07-24 17:09:35 -0400

    Completed exercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
+    let total = 0;
+    let numStart = 0;
+    let numEnd = 0;
+
+    if (numOne < 0 || numTwo < 0 || typeof(numOne) != 'number' || typeof(numTwo) != 'number' ) {
+        return "ERROR";
+    }
+
+    if (numOne < numTwo) {
+        numStart = numOne;
+        numEnd = numTwo;
+    } else {
+        numStart = numTwo;
+        numEnd = numOne;
+    }
+
+    while (numStart <= numEnd) {
+        total += numStart;
+        numStart++;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
