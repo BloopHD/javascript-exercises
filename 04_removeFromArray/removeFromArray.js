cef899f4ddf56700ccdd40a4a00fdc7d15c56db4
BloopHD <mrobcarlton@gmail.com>
2023-07-24 16:48:58 -0400

    Completed exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    // We start at i = 1 b/c the array is at i = 0.
+    for (let i = 1; i < arguments.length; i++) {
+
+        if (array.includes(arguments[i])) {
+
+            array.splice(array.findIndex((elm) => elm == arguments[i]), 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
