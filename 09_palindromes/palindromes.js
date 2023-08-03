a2085ffd2ab540ae978e2de5b6c179eec6d6ac22
Mickey Carlton <mrobcarlton@gmail.com>
2023-08-03 13:19:56 -0400

    Completed exercise 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    let cleanedUpWord = cleanUpWord(word);
+    let reversedWord = reverseWord(cleanedUpWord);  
+
+    return cleanedUpWord == reversedWord;
 };
+
+// Cleans up word by removing punctuation and spaces, and then converts to lowercase.
+function cleanUpWord(word){
+
+    let undesirables = /[\s\.,?!]/g;
+    let noUndesirablesWord = word.replace(undesirables, '');
+    let lowerCaseWord = noUndesirablesWord.toLowerCase();
+
+    return lowerCaseWord;
+}
+
+// Reverses the word, should be used after clean up.
+function reverseWord(word) {
+
+    let reversedWord = word.split('').reverse().join('');
+
+    return(reversedWord);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
