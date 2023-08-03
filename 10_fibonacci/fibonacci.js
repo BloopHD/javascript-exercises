2529b37923c3e94587b5079f8a10b77b8b4efac2
Mickey Carlton <mrobcarlton@gmail.com>
2023-08-03 13:34:26 -0400

    Completed exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let count = 1 
+    let a = 1;
+    let b = 0;
+
+    if (num > 0) {
+
+        while (count < num) {
+            count++;
+            let c = a
+            a += b;
+            b = c;
+        } 
+    
+        return a;
+    } else {
+
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
