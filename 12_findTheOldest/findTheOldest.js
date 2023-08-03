cb0a0d0545395ba3f104bab03b52cd7589a66ba9
Mickey Carlton <mrobcarlton@gmail.com>
2023-08-03 14:02:22 -0400

    Completed exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    return arr.sort((a, b) => getAge(a) < getAge(b) ? 1 : -1)[0];
 };
+
+function getAge(person) {
+
+    if (!person.yearOfDeath) {
+
+        return (new Date()).getFullYear() - person.yearOfBirth;
+    } else {
+        
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
