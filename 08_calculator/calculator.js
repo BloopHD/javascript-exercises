169a989dd3ea6efb1b6b5f514b348894a32ab369
Mickey Carlton <mrobcarlton@gmail.com>
2023-08-03 12:43:38 -0400

    Completed exercise 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => (total + num), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => (total === 0) ? total + num : total * num, 0);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let c = a;
+	while(b > 1) {
+    a *= c;
+    b--;
+  }
+  return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+  let b = 1;
+
+  while (a > 1) {
+    b *= a;
+    a--;
+  }
+
+  return b;
 };
 
 // Do not edit below this line
